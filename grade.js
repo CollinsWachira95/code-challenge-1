c60bfca1c90749e29250298af5f99af3c3bdc160
CollinsWachira95 <collins.wachira@student.moringaschool.com>
2024-07-22 14:55:06 +0300

    grade

--- grade.js
+++ grade.js
@@ -1,25 +1,30 @@
-//Deine Function
-function getStudentGrade(marks) {
-//Finding the grade by converting the marks to grades
-    let grade;
-    if (marks > 79){
-        grade = 'A';
+function calculateGrade() {
+    while (true) {
+      try {
+        const marks = parseInt(prompt("Enter student's marks (0-100): "));
+        if (marks >= 0 && marks <= 100) {
+          let grade;
+          if (marks >= 80) {
+            grade = "A";
+          } else if (marks >= 60) {
+            grade = "B";
+          } else if (marks >= 49) {
+            grade = "C";
+          } else if (marks >= 40) {
+            grade = "D";
+          } else {
+            grade = "E";
+          }
+          console.log(`Student's grade: ${grade}`);
+          break;
+        } else {
+          console.error("Invalid marks. Please enter a number between 0 and 100.");
+        }
+      } catch (error) {
+        console.error("Invalid input. Please enter an integer.");
+      }
     }
-    else if (marks >= 60){
-        grade = 'B';
-    }
-    else if (marks >= 50){
-        grade = 'C';
-    }
-    else if (marks >= 40){
-        grade = 'D';
-    }
-    else {
-        grade = 'E';
-    }
-//The Results
-    console.log(`The grades for the marks ${marks}is : ${grade}`);
-}
-
-getStudentGrade(85);
-getStudentGrade(45);
+  }
+  
+  calculateGrade();
+  
